0b862cb15d94bd8f4909b6602dee29f6affb338a
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-06-17 18:13:22 +0600

    auth error remove

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,6 +7,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db();
 export const auth = betterAuth({
+  secret: process.env.Better_AUTH_SECRET,
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
